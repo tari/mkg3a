a474947cebcf9543bf524ee6f222d296ecdbb310
Peter Marheine <peter@taricorp.net>
2012-01-24 14:31:12 -0500

    Preliminary (untested) libpng icon support.

--- icon.c
+++ icon.c
@@ -7,25 +7,101 @@
 #include "icon.h"
 #include "util.h"
 
-static char *bmperror;
+#if USE_PNG
+#include <png.h>
+#endif
 
-#if USE_MAGICKCORE
-#error MagickCore support not implemented.
-#else
+static char *bmperror;
 
 #define BMPFAIL(s) { bmperror = s; return 1; }
 
 u16 *loadBitmap(const char *path, int32_t *width, int32_t *height) {
-	struct bmp_header *bh;
-	struct dib_header *dh;
-	int err = 0;
-	u8 *data;
-
+    u16 *imgData = NULL;
 	FILE *fp = fopen(path, "rb");
 	if (fp == NULL) {
 		printf("Unable to open image file: %s\n", strerror(errno));
 		return NULL;
 	}
+
+#if USE_PNG
+    // Is this a PNG file?
+    char header[8];
+    fread(header, 1, 8, fp);
+    if (png_sig_cmp(header, 0, 8)) {
+        // Yes, load it up
+        rewind(fp);
+        u16 *imgData = loadBitmap_PNG(fp, width, height);
+    }
+#endif /* USE_PNG */
+
+    if (!imgData) {
+        loadBitmap_BMP(fp, width, height);
+    }
+    fclose(fp);
+    return convertBPP(imgData);
+}
+
+u16 *loadBitmap_PNG(FILE *fp, int32_t *width, int32_t *height) {
+    u16 *imageData = NULL;
+
+    // Allocate basic libpng structures
+    png_structp png_ptr = png_create_read_struct(
+        libpng_version, (png_voidp)user_error_ptr,
+        user_error_fn, user_warning_fn);
+    if (!png_ptr) {
+        fprintf("Failed to allocate memory for image data.", stderr);
+        return NULL;
+    }
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr) {
+        fprintf("Failed to allocate memory for image info struct.", stderr);
+        goto cleanup_png_ptr;
+    }
+    if (setjpm(png_jmpbuf(png_ptr))) {
+        // Libpng error
+        goto cleanup;
+    }
+
+    png_init_io(png_ptr, fp);
+    imageData = PNG_readImageData(png_ptr, int32_t *width, int32_t *height);
+
+cleanup:
+cleanup_png_ptr:
+    png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+    return imageData;
+}
+
+u16 *PNG_readImageData(png_structp png_ptr, png_infop info_ptr,
+                       int32_t *width, int32_t *height) {
+    png_bytep *row_pointers;
+    int32_t bit_depth, color_type;
+
+    *width = png_get_image_width(png_ptr, info_ptr);
+    *height = png_get_image_height(png_ptr, info_ptr);
+
+    png_read_png(png_ptr, info_ptr, 0, NULL);
+    row_pointers = png_get_rows(png_ptr, info_ptr);
+    bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+    color_type = png_get_color_type(png_ptr, info_ptr);
+
+    if (bit_depth != 8 || color_type != PNG_COLOR_TYPE_RGB) {
+        fprintf("Unsupported PNG bit depth or color type, must be RGB-8\n");
+        return NULL;
+    }
+
+    png_read_image(png_ptr, row_pointers);
+    u8 *imageData = mallocs(3 * width * height);
+    for (int y = 0; y < height; y++) {
+        memcpy(imageData, row_pointers[y], width * 3);
+    }
+    return imageData;
+}
+
+u16 *loadBitmap_BMP(FILE *fp, int32_t *width, int32_t *height) {
+	struct bmp_header *bh;
+	struct dib_header *dh;
+	int err = 0;
+	u8 *data;
 
 	bh = mallocs(sizeof(*bh));
 	dh = mallocs(sizeof(*dh));
@@ -46,8 +122,7 @@ u16 *loadBitmap(const char *path, int32_t *width, int32_t *height) {
 		return NULL;
 	}
 
-	fclose(fp);
-	return convertBPP(data);
+	return data;
 }
 
 /*
@@ -104,6 +179,7 @@ int readBMPData(u8 *d, FILE *fp) {
 	int row;
 	size_t sz;
 
+	// XXX hardcoded dimensions are begging to explode
 	for (row = ICON_HEIGHT - 1; row >= 0; row--) {
 		// Rows are 2208 bytes wide, so no alignment to worry about
 		// (rows are supposed to be aligned to 4 bytes)
